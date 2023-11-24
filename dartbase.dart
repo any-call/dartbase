51d77ad15a3026c7585872af00e813b7d7ecdeb2
jinguihua <156711203@qq.com>
2023-11-24 09:50:20 +0800

    changed

---
+++ dartbase.dart
@@ -0,0 +1,5 @@
+library dartbase;
+
+export 'lib/mynet/base.dart';
+export 'lib/mynet/base.exception.dart';
+export 'lib/mynet/dio.manager.dart';
--- lib/dartbase.dart
+++
@@ -1,5 +0,0 @@
-library dartbase;
-
-export 'mynet/base.dart';
-export 'mynet/base.exception.dart';
-export 'mynet/dio.manager.dart';
