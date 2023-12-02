7c81d0a8f5b09b809d81841f772f55d7543a5e20
jinguihua <156711203@qq.com>
2023-12-02 16:22:20 +0800

    changed

--- lib/myutils/base.dart
+++ lib/myutils/base.dart
@@ -1,3 +1,20 @@
+import 'dart:core';
+
 class MyUtils {
   static T? ambiguate<T>(T? value) => value;
+
+  ///C语言 字符 与 dart 字符串相互转换
+  // static String? toString(Pointer<Int8> str) {
+  //
+  //   return str.value.cast<utf8>().toDartString() ;
+  // }
+
+  // static Pointer<Uint8> toCPtr(String string) {
+  //   Uint8List units = Utf8Encoder().convert(string) ;
+  //
+  //
+  //
+  //
+  //   return utf8.decode(string.codeUnits)
+  // }
 }
