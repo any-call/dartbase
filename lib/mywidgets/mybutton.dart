433d3a20d0ff9634c1ea11dfc2f29bbed7f1d802
jinguihua <156711203@qq.com>
2023-12-04 16:48:44 +0800

    changed

--- lib/mywidgets/mybutton.dart
+++ lib/mywidgets/mybutton.dart
@@ -4,6 +4,15 @@ class MyButtonEx<T extends ButtonStyleButton> {
   late T _button;
   MyButtonEx();
 
+  void setColor(Color color) {
+    if (_button.style == null) {
+      _button.style =
+          ButtonStyle(foregroundColor: MaterialStateProperty.all(color));
+    } else {
+      _button.style?.foregroundColor = MaterialStateProperty.all(color);
+    }
+  }
+
   ///OutlinedButton
   static MyButtonEx<OutlinedButton> outlineButton(
       {VoidCallback? onPressed, required Widget child}) {
